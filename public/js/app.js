e77b93a55c9651a8984e17390d984ff76603f925
himal.bhandari@ioanyt.com <himal.bhandari@ioanyt.com>
2021-06-18 11:28:36 +0530

    add more weather related data

--- public/js/app.js
+++ public/js/app.js
@@ -7,17 +7,25 @@ const messageTwo = document.querySelector('#message-2')
 weatherForm.addEventListener('submit', (event) => {
     event.preventDefault()
     let location = search.value
-    messageOne.textContent = 'Loading....'
-    messageTwo.textContent = ''
+    messageOne.innerHTML = '<strong>LOADING...</strong>'
+    messageTwo.innerHTML = '<p></p>'
     fetch('/weather?address=' + location).then(response => {
         response.json().then(data => {
             if (data.error) {
                 messageOne.textContent = data.error
             } else {
-                messageOne.textContent = 'Location: ' + data.location.address
-                messageTwo.textContent = 'Temperature Details >>>  ' +
-                    'current ' + data.forecast.current + ' C' + ' and feels like ' +
-                    data.forecast.feels + ' C';
+                messageOne.innerHTML = '<h3>Showing Weather Data of <strong>' + data.forecast.region +
+                    '</strong> in Country <strong>' + data.forecast.country + '</strong></h3>' +
+                    '<p>Location Time Zone <strong>' + data.forecast.time_zone + '</strong> ' +
+                    'observation time <strong>' + data.forecast.observation_time + '</strong></p>'
+                messageTwo.innerHTML = '<h4>----Weather Details----<h4>' +
+                    '<p>Current Temp <strong>' + data.forecast.current + '</strong> &#8451; </p>' +
+                    '<p>Feels Like <strong>' + data.forecast.feels + '</strong> &#8451;</p>' +
+                    '<p>Weather Description: ' + data.forecast.weather_descriptions.join() + '</p>' +
+                    '<h4>----Wind Details----</h4>' + '<p>Wind Speed <strong>' + data.forecast.wind_speed + '</strong></p>' +
+                    '<p>Wind Direction <strong>' + data.forecast.wind_dir + '</strong></p>' +
+                    '<h4>----Humidity/Cloud/Visibility Details----</h4>' +
+                    '<p>Humidity: ' + data.forecast.humidity + ' Cloud Cover: ' + data.forecast.cloudcover + ' Visibility: ' + data.forecast.visibility + '</p>';
             }
         })
     })
--- src/utils/forcast.js
+++ src/utils/forcast.js
@@ -15,7 +15,17 @@ const weather = (latitude, longitude, callback) => {
         else {
             callback(undefined, {
                 current: body.current.temperature,
-                feels: body.current.feelslike
+                feels: body.current.feelslike,
+                weather_descriptions: body.current.weather_descriptions,
+                wind_speed : body.current.wind_speed,
+                wind_dir : body.current.wind_dir,
+                humidity: body.current.humidity,
+                cloudcover: body.current.cloudcover,
+                visibility: body.current.visibility,
+                observation_time: body.current.observation_time,
+                region: body.location.region,
+                country: body.location.country,
+                time_zone: body.location.timezone_id,
             })
         }
     })
